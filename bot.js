b0922af96a608d323606aa3792d507c92e5cf3d6
MrSlowpoke <maksimzlt@gmail.com>
2019-09-27 22:44:59 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const Canvas = require('canvas');
 const client = new Discord.Client();
 var auth = require('./auth.json');
 var key = "your lol api key";
@@ -161,6 +162,10 @@ function ChIDToName(id)
 }
 
 client.on('message', msg => {
+	const withoutPrefix = msg.content.slice(1);
+	const split = withoutPrefix.split(/ +/);
+	const command = split[0];
+	const args = split.slice(1);
   if (msg.content === '*roll') {
 	var number= Math.random()*champs1.length;
     msg.reply(champs1[Math.floor(number)], {files:["https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/"+champs2[Math.floor(number)]+".png"]});
@@ -188,6 +193,75 @@ client.on('message', msg => {
       msg.reply('You need to join a voice channel first!');
     }
   }
+   if (msg.content === 'комар') {
+	   client.channels.get('237175686903627777').send('<@!231080431771058176>');
+  }
+  function getUserFromMention(mention) {
+	// The id is the first and only match found by the RegEx.
+	const matches = mention.match(/^<@!?(\d+)>$/);
+
+	// If supplied variable was not a mention, matches will be null instead of an array.
+	if (!matches) return;
+
+	// However the first element in the matches array will be the entire mention, not just the ID,
+	// so use index 1.
+	const id = matches[1];
+
+	return client.users.get(id);
+}
+
+  async function f(){
+	const canvas = Canvas.createCanvas(470, 470);
+	const ctx = canvas.getContext('2d');
+	let user;
+	if(msg.content[8]!='<')
+		user=msg.author;
+	else
+	{
+	if (args[0]) {
+		user = getUserFromMention(args[0]);
+		if (!user) {
+			return message.reply('Please use a proper mention if you want to see someone else\'s avatar.');
+		}
+	}
+	}
+	//client.channels.get('626998541025411086').send(name);
+	const background = await Canvas.loadImage('./dog.png');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	// Pick up the pen
+	ctx.beginPath();
+	// Start the arc to form a circle
+	//ctx.arc(475, 475, 500, 0, Math.PI * 2, true);
+	ctx.save();
+	ctx.arc(365,375, 95, 0, Math.PI * 2, true);
+	// Put the pen down
+	ctx.closePath();
+	ctx.clip();
+	
+	// Clip off the region you drew on
+
+	const avatar = await Canvas.loadImage(user.avatarURL);
+	ctx.drawImage(avatar, 270, 280, 190, 190);
+	ctx.restore();
+	const background1 = await Canvas.loadImage('./hand.png');
+	ctx.drawImage(background1, 0, 0, canvas.width, canvas.height);
+	let todd = client.users.get('461265486655520788');
+	if(user===todd)
+	{
+	const background2 = await Canvas.loadImage('./toddskyrim.png');
+	ctx.drawImage(background2, 0, 0, canvas.width, canvas.height);
+	}
+	const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
+
+	msg.channel.send(attachment);
+	  
+  }
+
+  if (msg.content.includes('*avatar')) {
+	  f();
+}
+
   if (msg.content === '*kebab') {
     // Only try to join the sender's voice channel if they are in one themselves
     if (msg.member.voiceChannel) {
